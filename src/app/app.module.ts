ca656922a420317cc7246fc0bc7644be385d657e
sagithiyan <sagithiyannagendran@gmail.com>
2020-08-27 20:07:00 +0530

    new component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { from } from 'rxjs';
-
+import {ServerComponent} from './server/server.component';
+import { PracticeComponent } from './practice/practice.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    PracticeComponent
   ],
   imports: [
     BrowserModule,
